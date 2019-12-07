463b85c022ecd96f8c964ba655b0e27d26b0127a
Alan <alan@alan-desktop>
2019-12-07 16:05:00 -0600

    Day 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,3 +1,11 @@
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut sum : i64 = 0;
+    for line in stdin.lock().lines() {
+      let val : i64 = line.unwrap().parse::<i64>().unwrap();
+      sum += (val / 3) - 2;
+    }
+    println!("{}", sum);
 }
